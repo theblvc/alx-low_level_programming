6cd08b62cbf40b5611e523b24183413df22b36dd
theblvc <ckhoudinii@gmail.com>
2023-06-14 04:59:42 -0700

    this is an example

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer character by character.
+ * @n: The integer to be printed.
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+}
